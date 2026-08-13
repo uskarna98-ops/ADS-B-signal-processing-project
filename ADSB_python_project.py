a2d0c6f5ee5388598234250f9b1f7039374bafb0
Uttakarsh Karna <uskarna98@gmail.com>
2026-08-13 17:18:45 -0400

    Add files via upload
    In this file there are the codes used in this program.

---
+++ ADSB_python_project.py
@@ -0,0 +1,12 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#Uttakarsh Karna
+
+#this project is to run ADS-B 
+#we will begin by importing a dataset
+
+import scipy
+import scipy.io
+from scipy.io import wavfile
+import numpy as np
+import wave
